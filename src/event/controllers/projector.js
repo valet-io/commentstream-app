067c3478698ba99d7f8ababdc4f87ef8fcb0ea98
Ben Drucker <bvdrucker@gmail.com>
2014-09-15 12:47:52 -0400

    Design projector screen with static messages

--- src/event/controllers/projector.js
+++ src/event/controllers/projector.js
@@ -3,13 +3,19 @@
 module.exports = function ($scope) {
   $scope.messages = [
     {
-      message: 'Hi there!'
+      message: 'Hi there! This is a comment that is up on the screen!'
     },
     {
-      message: 'Well hello! Nice to be chatting with the room.'
+      message: 'Truly. This next comment is much longer. In fact, it is as long as text messages can be: 160 total characters. Any more and the phone will split it into parts. '
     },
     {
-      message: 'Welcome everyone!'
+      message: 'This is a bit more reasonable in length. The average message is like this. Two to three short sentences.'
+    },
+    {
+      message: 'Another message in between to force the last off my MBP screen.'
+    },
+    {
+      message: 'This last one (the oldest) runs just slightly off screen. Its contents are cut off eventually, but the first two lines show up on screen.'
     }
   ];
 };
